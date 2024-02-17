43d368fbb086185c40fe9e3343843c8d5f31e4a3
sanjuprakash01 <sanjuprakash6001@gmail.com>
2024-02-17 21:17:03 +0530

    updations

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 $(document).ready(function () {
   $('#btn').on('click', function () {
-    $('.sidebarMenu').toggleClass('active');
+    if ($(window).width() > 992) {
+      $('.sidebarMenu').toggleClass('active');  
+    } else {
+      $('.sidebarMenu').toggleClass("open");
+    }
+  });
+
+  $('#btn2').on('click', function() {
+    $('.sidebarMenu').removeClass('active');  
+  })
+  $('#menuBtn').on('click', function (e) {
+    e.preventDefault();
+    $('.sidebarMenu').toggleClass('open');
+  });
+
+  $('#closeBtn').on('click', function (e) {
+    e.preventDefault();
+    $('.sidebarMenu').removeClass('open');
   });
 });
-
 
 function createCol(imageSrc, title, description) {
   const colHtml = `
@@ -13,7 +29,7 @@ function createCol(imageSrc, title, description) {
         </div>
         <div class="content">
           <p class="title">${title}</p>
-          <p class="subText text-gray">${description}</p>
+          <p class="subText mt-1">${description}</p>
         </div>
       </div>
     `;
@@ -23,11 +39,11 @@ function createCol(imageSrc, title, description) {
 const $contentContainer = $('#contentContainer');
 
 $contentContainer.append(createCol('assets/City-image.png', 'Alexandria', 'Best Hotel, Villa'));
-$contentContainer.append(createCol('assets/City-image.png', 'Astana', 'Best Hotel, Villa'));
-$contentContainer.append(createCol('assets/City-image.png', 'Calgary', 'Best Hotel, Villa'));
-$contentContainer.append(createCol('assets/City-image.png', 'Fairblanks', 'Best Hotel, Villa'));
-$contentContainer.append(createCol('assets/City-image.png', 'Jackson', 'Best Hotel, Villa'));
-$contentContainer.append(createCol('assets/City-image.png', 'Madison', 'Best Hotel, Villa'));
-$contentContainer.append(createCol('assets/City-image.png', 'Monterrey', 'Best Hotel, Villa'));
-$contentContainer.append(createCol('assets/City-image.png', 'Palo Alto', 'Best Hotel, Villa'));
-$contentContainer.append(createCol('assets/City-image.png', 'Sevilla', 'Best Hotel, Villa'));
+$contentContainer.append(createCol('assets/Astana.png', 'Astana', 'Best Hotel, Villa'));
+$contentContainer.append(createCol('assets/Calgary.png', 'Calgary', 'Best Hotel, Villa'));
+$contentContainer.append(createCol('assets/Fairbanks.png', 'Fairblanks', 'Best Hotel, Villa'));
+$contentContainer.append(createCol('assets/Jackson.png', 'Jackson', 'Best Hotel, Villa'));
+$contentContainer.append(createCol('assets/Madison.png', 'Madison', 'Best Hotel, Villa'));
+$contentContainer.append(createCol('assets/Monterrey.png', 'Monterrey', 'Best Hotel, Villa'));
+$contentContainer.append(createCol('assets/Palo.png', 'Palo Alto', 'Best Hotel, Villa'));
+$contentContainer.append(createCol('assets/Seville.png', 'Sevilla', 'Best Hotel, Villa'));
